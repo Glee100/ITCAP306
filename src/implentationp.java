62a6bbe7017d72534bf4468dcbba30e53bd4494d
Gene <glee@commonedge.com>
2018-04-23 22:32:49 -0400

    sd

--- src/implentationp.java
+++ src/implentationp.java
@@ -1,5 +1,6 @@
 import java.util.Map;
 import java.util.*;
+import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
 /**
@@ -16,25 +17,30 @@ public class implentationp
 	 //map with login as key, and arraylist of customer and accounts as value
 	 Map<Login, Vector<Object> > logins = new HashMap<Login, Vector<Object>>();
 	 
-	 //populate the map
+	 //create customers
 	 Customer customer1 = new Customer("Yamil","Castro","Manassas","123","y@gmu.edu");
 	 Customer customer2 = new Customer("Gene","Lee","Manassas","124","g@gmu.edu");
 	 Customer customer3 = new Customer("John","Smith","Manassas","125","j@gmu.edu");
 	 
+	 //populate the map
 	 logins.put(new Login("customer1", "123"),new Vector<Object>(){{
 		    add(customer1);
 		    add(new Checking(customer1, "12/12/12", new Transaction("12/12/12",600)));
 		    add(new Savings(customer1, "12/13/12", new Transaction("12/13/12",800)));
-		    add(new MarketAccount(customer,"12/14/12"));
+		    add(new MarketAccount(customer1,"12/14/12"));
 		}});
 	 
 	 logins.put(new Login("customer2", "124"),new Vector<Object>(){{
-		    add(customer1);
-		    add(new MarketAccount(customer,"12/14/12"));
+		    add(customer2);
+		    add(new MarketAccount(customer2,"12/14/12"));
 		}});
 	 
-
-	 logins.put(new Login("customer3", "125"),customer3);
+	 logins.put(new Login("customer3", "125"),new Vector<Object>(){{
+		    add(customer3);
+		    add(new Checking(customer3, "12/12/12", new Transaction("12/12/12",600)));
+		    add(new Savings(customer3, "12/13/12", new Transaction("12/13/12",800)));
+		}});
+	 
 
 	 
 	 boolean validLogin = false;
@@ -47,20 +53,25 @@ public class implentationp
 		 
 		 if(!validLogin)
 		 {
-			 JOptionPane.showInputDialog("You're login is incorrect. Would you like to try again? 'Y' or 'N'");
+			 String again = JOptionPane.showInputDialog("You're login is incorrect. If you don't want to try again, enter 'N'.");
+			 if(again.equalsIgnoreCase("N")) {break;}
 		 }
 	 }
 	 
-	 
+	 //login must be valid in order to access the program
+	 if(validLogin)
+	 {
+		 Menu();
+	 }
 	 
      //if true
      //customer1, john, etc,
      //tokens
-     Customer c = new Customer (//get data from file);
+     //Customer c = new Customer (//get data from file);
      //menu
-     Account s = new Checkings(///);
+    // Account s = new Checkings(///);
      // for the other ones
-     print(linkedlist);
+    // print(linkedlist);
      
     }
  // change parameters
@@ -71,20 +82,43 @@ public class implentationp
 
 	/* Account a = new Checking(c,new Transaction(JOptionPane.showInputDialog(null,"Transaction Date")), 
 			     JOptionPane.showInputDialog(null,"Enter deposit"));	*/
+	 
+	 
+	 /*
 	 Account a = new Checking(customer, "12/12/12", new Transaction("12/12/12",600));
 	 Account b = new Savings(customer, "12/13/12", new Transaction("12/13/12",800));
 	 Account c = new MarketAccount(customer,"12/14/12");
+	 */
 	 
-	 
- 	return a;
+ 	return null;
  }
- public static void Menu(///) {
+ 
+ public static void Menu() {
+		 
+		 ArrayList<String> optionList = new ArrayList<String>();
+		
+		 optionList.add("1");
+		 optionList.add("2");
+
+		 Object[] options = optionList.toArray();
+		 int value = JOptionPane.showOptionDialog(
+		                 null,
+		                 "Please select your encryption Algorithm:(1 or 2)\n 1. Data Encryption       Standard(DES) \n 2. Advanced Encryption Standard(AES)",
+		                 "Pick",
+		                 JOptionPane.YES_NO_OPTION,
+		                 JOptionPane.QUESTION_MESSAGE,
+		                 null,
+		                 options,
+		                 optionList.get(0));
+
+		 String opt = optionList.get(value);
+		 System.out.println("You picked " + opt);
  }
 
 /*
  * Static method that handles logins
  * */
-public static boolean Login(Map<Login, Customer> logins)
+public static boolean Login(Map<Login, Vector<Object>> logins)
 {
 	 String username = JOptionPane.showInputDialog("Enter Username: ");
 	 String password = JOptionPane.showInputDialog("Enter password: ");

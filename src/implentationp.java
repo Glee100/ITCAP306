d995cdd45c8dae02271e3c8c410afd847d2280db
ycastroa <38576680+ycastroa@users.noreply.github.com>
2018-04-23 21:59:18 -0400

    everything

--- src/implentationp.java
+++ src/implentationp.java
@@ -12,10 +12,16 @@ public class implentationp
 {
     // instance variables - replace the example below with your own
  public static void main(String[] args){
-	 Map<String, Login> logins = new HashMap<String, Login>();
-	 logins.put("customer1", new Login("customer1", "123"));
-	 logins.put("customer2", new Login("customer2", "124"));
-	 logins.put("customer3", new Login("customer3", "125"));    
+	 Map<Login, Customer > logins = new HashMap<Login, Customer>();
+	 
+	 Customer customer1 = new Customer("Yamil","Castro","Manassas","123","y@gmu.edu");
+	 Customer customer2 = new Customer("Gene","Lee","Manassas","124","g@gmu.edu");
+	 Customer customer3 = new Customer("John","Smith","Manassas","125","j@gmu.edu");
+	 
+	 logins.put(new Login("customer1", "123"),customer1 );
+	 logins.put(new Login("customer2", "124"),customer2);
+	 logins.put(new Login("customer3", "125"),customer3);
+
      //if true
      //customer1, john, etc,
      //tokens
@@ -27,19 +33,21 @@ public class implentationp
      
     }
  // change parameters
- public static Account createAccount(String[] Array) {
+ public static Account[] createAccount(Customer customer) {
 	 /*Customer c = new Customer(JOptionPane.showInputDialog(null,"First Name"),JOptionPane.showInputDialog(null,"Last Name"), 
 			 					JOptionPane.showInputDialog(null,"Address"), JOptionPane.showInputDialog(null,"Phone"), 
 			 							JOptionPane.showInputDialog(null,"email"));*/
-	 Customer customer = new Customer("Yamil","Castro","Manassas","123","y@gmu.edu");
-	 
+
 	/* Account a = new Checking(c,new Transaction(JOptionPane.showInputDialog(null,"Transaction Date")), 
 			     JOptionPane.showInputDialog(null,"Enter deposit"));	*/
-	 Account a = new Checking(customer, "12/12/12", new Transaction("12/12/12",100));
+	 Account a = new Checking(customer, "12/12/12", new Transaction("12/12/12",600));
+	 Account b = new Savings(customer, "12/13/12", new Transaction("12/13/12",800));
+	 Account c = new MarketAccount(customer,"12/14/12");
+	 
+	 
  	return a;
  }
- public static void Menu(String[] Array) {
-	 
+ public static void Menu(///) {
  }
  
 }
